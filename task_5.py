d526f51842d06ba4c445d16d37fb61a5789e03b6
WheatOlga <pon88@list.ru>
2026-03-16 20:45:38 +0300

    add files for Sprint_2

---
+++ task_5.py
@@ -0,0 +1,48 @@
+class Results:
+    def __init__(self, victories, draws, losses):
+        self.victories = victories
+        self.draws = draws
+        self.losses = losses
+
+class Football(Results):
+    def number_of_wins(self):
+        return f'Футбольных побед: {self.victories}'
+    
+    def number_of_draws(self):
+        return f'Футбольных ничьих: {self.draws}'
+    
+    def number_of_losses(self):
+        return f'Футбольных поражений: {self.losses}'
+    
+    def total_points(self):
+        points = 3*self.victories + self.draws
+        return f'Общее количество очков: {points}'
+    
+class Hockey(Results):
+    def number_of_wins(self):
+        return f'Хоккейных побед: {self.victories}'
+    
+    def number_of_draws(self):
+        return f'Хоккейных ничьих: {self.draws}'
+    
+    def number_of_losses(self):
+        return f'Хоккейных поражений: {self.losses}'
+    
+    def total_points(self):
+        points = 2*self.victories + self.draws
+        return f'Общее количество очков: {points}'
+    
+football_team = Football(2, 2, 2)
+hockey_team = Hockey(2, 2, 2)
+
+for football_method in [football_team.number_of_wins, 
+                        football_team.number_of_draws, 
+                        football_team.number_of_losses, 
+                        football_team.total_points]:
+    print(football_method())
+
+for hockey_method in [hockey_team.number_of_wins, 
+                      hockey_team.number_of_draws, 
+                      hockey_team.number_of_losses, 
+                      hockey_team.total_points]:
+    print(hockey_method())
